177d7679729c17310c2a2584714c5a3d2a1dd165
abhiscs <abhiscs@gmail.com>
2024-01-03 04:40:33 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami="ami-079db87dc4c10ac91"
+  instance_type="t2.micro"
+
+  tags = {
+    Name="tf-example"
+  }
+}
