b730497506b83d684f1c9be569222aecfbc26f23
abhiscs <abhiscs@gmail.com>
2024-01-26 20:27:00 -0500

    Added: Ch2 - Deploying a Cluster of Web Servers

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0a3c3a20c09d6f377"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  key_name               = aws_key_pair.aws-key.key_name
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  key_name        = aws_key_pair.aws-key.key_name
 
-  tags = {
-    Name = "terraform-example"
-  }
+  # tags = {
+  #   Name = "terraform-example"
+  # }
 
   connection {
     type        = "ssh"
@@ -29,9 +29,111 @@ resource "aws_instance" "example" {
                 service httpd start
                 EOF
 
-  user_data_replace_on_change = "true"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404:->Page Not Found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+}
+
+resource "aws_security_group" "alb" {
+  name = "terrafrom-example-alb"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 4
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -73,7 +175,7 @@ variable "server_port" {
   default     = 80
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The Public IP of the Sharon Webserver"
+output "alb_dns_name"{
+  value=aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
