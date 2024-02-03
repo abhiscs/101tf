9d9c8d310643435679a70c1dbca82b1e1b34a953
abhiscs <abhiscs@gmail.com>
2024-02-02 19:39:17 -0500

    web-server-cluster

--- main.tf
+++ main.tf
@@ -1,25 +1,22 @@
-terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0277155c3f0ab2930"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0277155c3f0ab2930"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  key_name        = aws_key_pair.aws-key.key_name
 
-  tags = {
-    Name = "terraform-example"
+  # tags = {
+  #   Name = "terraform-example"
+  # }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
   }
 
   user_data = <<-EOF
@@ -27,19 +24,147 @@ resource "aws_instance" "example" {
                 sudo -i
                 yum update -y
                 yum install httpd -y
-                sudo systemctl start httpd
                 touch /var/www/html/index.html
                 echo "Hello, Sharon!" > /var/www/html/index.html
+                service httpd start
                 EOF
-  user_data_replace_on_change = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terrafrom-example-instance"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404:->Page Not Found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+}
+
+resource "aws_security_group" "alb" {
+  name = "terrafrom-example-alb"
   ingress {
-    from_port   = var.server_serverport
-    to_port     = var.server_serverport
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 4
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  #SSH
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #HTTP
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #Outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "aws-key" {
+  key_name   = "terraform-example-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
